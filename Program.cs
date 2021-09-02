94d16f81fa79d61d93f1f8d674b05221258baea9
Kwabena-Joseph Casimir <casimirk1@usf.edu>
2021-09-02 15:22:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string inp = Console.ReadLine();
+        int count = int.Parse(inp);
+        int iter = 1;
+
+        while (iter <= count)
+        {
+            Console.WriteLine(iter + " Abracadabra");
+            iter++;
+        }
+    }
+}
